d3049502b0e2c66dd247bcdd42f1e1bce8b16bdf
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-06-02 16:37:12 -0400

    Add tweets

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -56,5 +57,10 @@ class User extends Authenticatable
     public function follwers(): BelongsToMany
     {
         return $this->belongsToMany(User::class, 'followers', 'following_id', 'user_id');
+    }
+
+    public function tweetsFromFollowing(): HasManyThrough
+    {
+        return $this->hasManyThrough(Tweet::class, Follower::class, 'user_id', 'user_id', 'id', 'following_id');
     }
 }
--- database/migrations/2025_06_02_202458_create_tweets_table.php
+++ database/migrations/2025_06_02_202458_create_tweets_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('followers', function (Blueprint $table) {
+        Schema::create('tweets', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('following_id')->constrained('users')->onDelete('cascade');
+            $table->text('body', 250);
             $table->timestamps();
         });
     }
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('followers');
+        Schema::dropIfExists('tweets');
     }
 };
