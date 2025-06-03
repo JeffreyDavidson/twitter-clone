8e92bfddaca15be7e65ef16c0d4b57cd51f4b5ce
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-06-03 12:10:30 -0400

    Remove text length constraint

--- database/migrations/2025_06_02_202458_create_tweets_table.php
+++ database/migrations/2025_06_02_202458_create_tweets_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('tweets', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->text('body', 250);
+            $table->text('body');
             $table->timestamps();
         });
     }
