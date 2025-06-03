f1a9c17f0b02351162da5b02c14d7727a8dd340a
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-06-02 21:54:05 -0400

    Send full object

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -17,7 +17,7 @@ defineProps({
                 nav
             </div>
             <div class="w-7/12 border border-gray-800 border-t-0 border-b-0">
-                <AppTimeline :tweets="tweets.data" />
+                <AppTimeline :tweets="tweets" />
             </div>
         </div>
     </AuthenticatedLayout>
