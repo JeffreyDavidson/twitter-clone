27c0c8311f3c28329640ca85c50aa8b96bf32366
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-06-03 21:12:24 -0400

    Update auth user after login

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -33,7 +33,9 @@ class HandleInertiaRequests extends Middleware
         return [
             ...parent::share($request),
             'auth' => [
-                'user' => new UserResource($request->user()),
+                'user' => fn () => $request->user()
+                    ? new UserResource($request->user())
+                    : null,
             ],
         ];
     }
